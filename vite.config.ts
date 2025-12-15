878763001ea914ee9431e26bd66edab8a51ad795
Felipe Fonseca <57119449+fonsecaprojetos@users.noreply.github.com>
2025-12-15 18:15:31 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Prevents "process is not defined" error in browser for legacy code reliance
+    'process.env': {}
+  },
   build: {
     outDir: 'dist',
     sourcemap: true
